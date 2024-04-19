497e1c14a16ce8b80f7a004bcc9a82cfb21e2060
Jacob Fratti <JacobRFratti@gmail.com>
2024-04-18 20:57:16 -0400

    Updated small changes to HTML and CSS

--- src/app/backlog/page.js
+++ src/app/backlog/page.js
@@ -64,9 +64,10 @@ export default function backlog() {
             <label for="horizontal-list-radio-rating" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Rating</label>
         </div>
     </li>
-    <li class="w-full dark:border-gray-600">
-        <div class="flex items-center text-center px-3">
-            <button class="w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300">No Sorting</button>
+    <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-l dark:border-gray-600">
+        <div class="flex items-center px-3">
+            <input id="horizontal-list-radio-date" type="radio" value="" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+            <label for="horizontal-list-radio-date" class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Release Date </label>
         </div>
     </li>
 </ul>
